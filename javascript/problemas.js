6cfb1c5a48c3d0761b45c8fb8d3c1ab83de9e4ba
estebanProjects <84748193+estebanProjects@users.noreply.github.com>
2021-05-29 00:50:33 -0500

    Add files via upload

---
+++ javascript/problemas.js
@@ -0,0 +1,155 @@
+let problema1 = {
+    imgProblema: "problemas/problema1_mejorado_arit.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion1.jpg"
+}
+
+let problema2 = {
+    imgProblema: "problemas/problema2_mejorado_arit.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion2.jpg"
+}
+
+let problema3 = {
+    imgProblema: "problemas/problema3_mejorado_arit.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion3.jpg"
+}
+
+let problema4 = {
+    imgProblema: "problemas/problema4_arit.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion4.jpg"
+}
+
+let problema5 = {
+    imgProblema: "problemas/problema5_arit.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion5.jpg"
+}
+
+
+
+let problema6 = {
+    imgProblema: "problemas/problema6_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion6.jpg"
+}
+
+let problema7 = {
+    imgProblema: "problemas/problema7_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion7.jpg"
+}
+
+let problema8 = {
+    imgProblema: "problemas/problema8_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion8.jpg"
+}
+
+let problema9 = {
+    imgProblema: "problemas/problema9_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion9.jpg"
+}
+
+let problema10 = {
+    imgProblema: "problemas/problema10_alg.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion10.jpg"    
+}
+
+let problema11 = {
+    imgProblema: "problemas/problema11_alg.jpg",
+    respuesta: "d",
+    imgResolucion: "resolucion/resolucion11.jpg"    
+}
+
+let problema12 = {
+    imgProblema: "problemas/problema12_alg.jpg",
+    respuesta: "d",
+    imgResolucion: "resolucion/resolucion12.jpg"    
+}
+
+let problema13 = {
+    imgProblema: "problemas/problema13_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion13.jpg"    
+}
+
+let problema14 = {
+    imgProblema: "problemas/problema14_alg.jpg",
+    respuesta: "a",
+    imgResolucion: "resolucion/resolucion14.jpg"    
+}
+
+let problema15 = {
+    imgProblema: "problemas/problema15_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion15.jpg"    
+}
+
+let problema16 = {
+    imgProblema: "problemas/problema16_alg.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion16.jpg"    
+}
+
+let problema17 = {
+    imgProblema: "problemas/problema17_geo.jpg",
+    respuesta: "d",
+    imgResolucion: "resolucion/resolucion17.jpg"    
+}
+
+
+let problema18 = {
+    imgProblema: "problemas/problema18_geo.jpg",
+    respuesta: "d",
+    imgResolucion: "resolucion/resolucion18.jpg"
+}
+
+let problema19 = {
+    imgProblema: "problemas/problema19_geo.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion19.jpg"
+}
+
+let problema20 = {
+    imgProblema: "problemas/problema20_geo.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion20.jpg"
+}
+
+let problema21 = {
+    imgProblema: "problemas/problema21_geo.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion21.jpg"
+}
+
+let problema22 = {
+    imgProblema: "problemas/problema22_geo.jpg",
+    respuesta: "a",
+    imgResolucion: "resolucion/resolucion22.jpg"
+}
+
+let problema23 = {
+    imgProblema: "problemas/problema23_geo.jpg",
+    respuesta: "c",
+    imgResolucion: "resolucion/resolucion23.jpg"
+}
+
+let problema24 = {
+    imgProblema: "problemas/problema24_geo.jpg",
+    respuesta: "a",
+    imgResolucion: "resolucion/resolucion24.jpg"
+}
+
+
+let problema25 = {
+    imgProblema: "problemas/problema25_trig.jpg",
+    respuesta: "b",
+    imgResolucion: "resolucion/resolucion25.jpg"
+}
+
+let problemasPucp = [problema1, problema2, problema3, problema4, problema5 ,problema6, problema7, problema8, problema9, problema10, problema11, problema12, problema13, problema14, problema15, problema16, problema17, problema18, problema19, problema20, problema21, problema22, problema23, problema24, problema25]
